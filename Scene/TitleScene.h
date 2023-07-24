b5f0563e0dd4336acee03433f1596afb10f01594
Rt-Bitbucket <210020@jc-21.jp>
2023-07-24 09:58:19 +0900

    シーンの追加、下準備中

---
+++ Scene/TitleScene.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../Engine/GameObject.h"
+
+class TitleScene : public GameObject
+{
+public:
+
+	TitleScene(GameObject* parent);
+	void Initialize() override;
+
+private:
+
+
+};
+
